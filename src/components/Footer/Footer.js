8bc47d24752065e1dcbae18b175457dc67d6de9d
Jonathon Temelkovski <jtemel98@gmail.com>
2021-07-27 14:21:14 -0400

    Update logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,6 +10,7 @@ import {
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import NoteIcon from '@material-ui/icons/Note';
 
 import styles from './Footer.module.css';
 
@@ -20,7 +21,7 @@ export default class Footer extends React.Component {
         <div className={styles.footer}>
           <div className={styles.footerLinks}>
             <Grid container justify="center" alignItems="center" spacing={0}>
-              <Grid item xs={6} >
+              <Grid item xs={4} >
                 <a
                   href="https://ca.linkedin.com/in/jonathon-temelkovski"
                   target="_blank"
@@ -31,7 +32,7 @@ export default class Footer extends React.Component {
                   </IconButton>
                 </a>
               </Grid>
-              <Grid item xs={6} >
+              <Grid item xs={4} >
                 <a
                   href="https://github.com/jtemel/"
                   target="_blank"
@@ -39,6 +40,17 @@ export default class Footer extends React.Component {
                 > 
                   <IconButton className={styles.cartButton} aria-label="github">
                     <GitHubIcon />
+                  </IconButton>
+                </a>
+              </Grid>
+              <Grid item xs={4} >
+                <a
+                  href="https://google.ca"
+                  target="_blank"
+                  rel="noreferrer"
+                > 
+                  <IconButton className={styles.cartButton} aria-label="resume">
+                    <NoteIcon />
                   </IconButton>
                 </a>
               </Grid>
